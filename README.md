01032fd302bc2eabf724038de4ee07d0b6b96342
Rizki Muh Pasinringi <rizkimuhpasinringi@gmail.com>
2023-01-24 19:11:23 +0800

    initial commits

---
+++ README.md
@@ -0,0 +1,5 @@
+# Multiplication App
+I'm trying to make a Multiplication App
+
+
+link Website : https://rizki-mp.github.io/Multiplication-App/
---
+++ script.js
@@ -0,0 +1,40 @@
+const score = document.getElementById('score')
+const numberOne = document.getElementById('number1')
+const numberTwo = document.getElementById('number2')
+const input = document.getElementById('input')
+const answer = document.getElementById('answer')
+const containerAnswer = document.getElementById('containerAnswer')
+
+const value1 = Math.ceil(Math.random() * 10)
+const value2 = Math.ceil(Math.random() * 10)
+
+const result = value2 * value1
+
+numberOne.innerHTML = value1
+numberTwo.innerHTML = value2
+
+let point = localStorage.getItem("result")
+
+if (!point) {
+	let point = 0
+}
+console.log(point)
+
+containerAnswer.addEventListener("click", () => {
+	const answer = parseInt(input.value)
+
+	if(answer ==  result) {
+		console.log("benar")
+		point++
+		localStorage.setItem("result", point)
+		location.reload();
+	} else {
+		point--
+		localStorage.setItem("result", point)
+		location.reload();
+	}
+
+
+} )
+
+score.innerHTML = localStorage.getItem("result")
